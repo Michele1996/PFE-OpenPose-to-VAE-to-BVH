a0a8cbc69c6cefa6a66be66f84d694612f5bf46e
Michele <michele.grimaldi@isir.upmc.fr>
2022-01-26 00:41:22 +0100

    Fix create npy file

--- create_npy.py
+++ create_npy.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import os
 import numpy as np
+import argparse
 
-def create_npy(path="./valid_outputs/",scene_len = None):
+def create_npy(path=".output/output_final/",scene_len=None):
     if scene_len == "max":
         maximum = 0
         for scene in os.listdir(path):
@@ -35,48 +36,17 @@ def create_npy(path="./valid_outputs/",scene_len = None):
             temp = np.zeros((maximum,50))
             temp[:len(kp_scene)] = kp_scene
             kp_scene = temp
-        
-
-        
         if np.size(kp_scene) > 0:
             kp_total.append(np.array(kp_scene))
-    #print(len(kp_total))
-    np.save("kp_none",np.array(kp_total,dtype=object))
+    np.save("kp_3",np.array(kp_total,dtype=object))
 
-create_npy()
 
-def create_new_npy(path="./output_f_json_folder/",scene_len = None):
-    if scene_len == "max":
-        maximum = np.max(len(os.listdir(path))-1)
-        print(maximum)
-        if maximum%2 == 1 :
-            maximum +=1
-    elif type(scene_len) == int :
-        maximum = scene_len
-        if maximum%2 == 1 :
-            maximum +=1
-    else :
-        maximum = -1
-
-    kp_scene = []
-    for frame in os.listdir(path):
-        #for frame in os.listdir(scene):
-        if frame[-4:]!='.mp4' and len(os.listdir(path)) > 0 :
-            df = pd.read_json(path+frame)
-            people = df["people"]
-            kp = people[0]["pose_keypoints_2d"]
-            kp = np.delete(kp,[i for i in range(2,len(kp),3)])
-            #print(kp)
-            kp_scene.append(np.array(kp))
-
-        if maximum >= 0 :
-            temp = np.zeros((maximum,50))
-            temp[:len(kp_scene)] = kp_scene
-            kp_scene = temp
-        
-
-        
-    print(kp_scene)
-    np.save("kp_new",np.array(kp_scene,dtype=object))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path_to_output", help="path to openpose json output",type=str)
+    parser.add_argument("len_scene", help="max length for each scene",type=int)
+    args = parser.parse_args()
+    nb_iter=args.nb_iter
+    create_npy(args.path_to_output, args.len_scene)
 
 
