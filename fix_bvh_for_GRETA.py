70fe1ca81ebac5778188ce4843453988af124ac8
Michele <michele.grimaldi@isir.upmc.fr>
2022-02-06 14:46:15 +0100

    clean and fix, bvh for greta

--- fix_bvh_for_GRETA.py
+++ fix_bvh_for_GRETA.py
@@ -4,7 +4,7 @@ import numpy
 old_bvh=open("out.bvh","r")
 new_bvh=open("new_bvh.bvh","w")
 l=old_bvh.readlines()
-not_to_change=["Hip","Knee","Ankle","Collar"]
+not_to_change=["Hip","Knee","Ankle","Collar","Foot"]
 lines=[]
 index=0
 motion_index=-1
@@ -40,33 +40,47 @@ for p in range(len(motion_lines)):
                    #print(motion_lines[p][j[1]*3+3+1])
                    #print(motion_lines[p][j[1]*3+3+2])
                    if("Shoulder" in j[0]):
-                       motion_lines[p][i[1]*3+3]=str(float(motion_lines[p][j[1]*3+3]))
-                       motion_lines[p][i[1]*3+3+1]=str(float(motion_lines[p][j[1]*3+3+1])+60)
-                       motion_lines[p][i[1]*3+3+2]=str(float(motion_lines[p][j[1]*3+3+2])-60)
-                       motion_lines[p][j[1]*3+3]=str(float(val_1)-60)
-                       motion_lines[p][j[1]*3+3+1]=str(float(val_2)-90)
-                       motion_lines[p][j[1]*3+3+2]=str(float(val_3)-30)
+                       motion_lines[p][i[1]*3+3]=str(round(float(motion_lines[p][j[1]*3+3]),2)+15)
+                       motion_lines[p][i[1]*3+3+1]=str(round(float(motion_lines[p][j[1]*3+3+1])+20,2))
+                       motion_lines[p][i[1]*3+3+2]=str(round(float(motion_lines[p][j[1]*3+3+2])-40,2))
+                       if(round(float(motion_lines[p][j[1]*3+3+2])-40,2)>-60):
+                          y=round(float(motion_lines[p][j[1]*3+3+2])-40,2)
+                          x=66+round(float(motion_lines[p][j[1]*3+3+2])-40,2)
+                          print(x,y)
+                          motion_lines[p][i[1]*3+3+2]=str(round(float(motion_lines[p][j[1]*3+3+2])-x-40,2))
+                       if(round(float(motion_lines[p][j[1]*3+3+1])+20,2)<60):
+                          y=round(float(motion_lines[p][j[1]*3+3+1])+20,2)
+                          x=67-round(float(motion_lines[p][j[1]*3+3+1])+20,2)
+                          print(x,y)
+                          motion_lines[p][i[1]*3+3+1]=str(round(float(motion_lines[p][j[1]*3+3+1])+x+20,2))
+                       motion_lines[p][j[1]*3+3]=str(round(float(val_1)-60,2))
+                       motion_lines[p][j[1]*3+3+1]=str(round(float(val_2)-90,2))
+                       motion_lines[p][j[1]*3+3+2]=str(round(float(val_3)-30,2))
                    elif("Elbow" in j[0]):
-                       motion_lines[p][i[1]*3+3]=str(float(motion_lines[p][j[1]*3+3])-30)
-                       motion_lines[p][i[1]*3+3+1]=str(float(motion_lines[p][j[1]*3+3+1])-45)
-                       motion_lines[p][i[1]*3+3+2]=str(float(motion_lines[p][j[1]*3+3+2])-70)
-                       motion_lines[p][j[1]*3+3]=str(float(val_1)-45)
-                       motion_lines[p][j[1]*3+3+1]=str(float(val_2)+120)
-                       motion_lines[p][j[1]*3+3+2]=str(float(val_3))
+                       motion_lines[p][i[1]*3+3]=str(round(float(motion_lines[p][j[1]*3+3])-30,2))
+                       motion_lines[p][i[1]*3+3+1]=str(round(float(motion_lines[p][j[1]*3+3+1])-45,2))
+                       motion_lines[p][i[1]*3+3+2]=str(round(float(motion_lines[p][j[1]*3+3+2])-50,2))
+                       motion_lines[p][j[1]*3+3]=str(round(float(val_1)-45,2))
+                       motion_lines[p][j[1]*3+3+1]=str(round(float(val_2)+130,2))
+                       motion_lines[p][j[1]*3+3+2]=str(round(float(val_3),2))
                    elif("Wrist"in j[0]):
-                       motion_lines[p][i[1]*3+3]=str(float(motion_lines[p][j[1]*3+3])-30)
-                       motion_lines[p][i[1]*3+3+1]=str(float(motion_lines[p][j[1]*3+3+1])+60)
-                       motion_lines[p][i[1]*3+3+2]=str(float(motion_lines[p][j[1]*3+3+2]))
-                       motion_lines[p][j[1]*3+3]=str(float(val_1))
-                       motion_lines[p][j[1]*3+3+1]=str(float(val_2))
-                       motion_lines[p][j[1]*3+3+2]=str(float(val_3))
-                   else:
-                       motion_lines[p][i[1]*3+3]=str(float(motion_lines[p][j[1]*3+3]))
-                       motion_lines[p][i[1]*3+3+1]=str(float(motion_lines[p][j[1]*3+3+1]))
-                       motion_lines[p][i[1]*3+3+2]=str(float(motion_lines[p][j[1]*3+3+2]))
-                       motion_lines[p][j[1]*3+3]=str(float(val_1))
-                       motion_lines[p][j[1]*3+3+1]=str(float(val_2))
-                       motion_lines[p][j[1]*3+3+2]=str(float(val_3))
+                       motion_lines[p][i[1]*3+3]=str(round(float(motion_lines[p][j[1]*3+3])-25,2))
+                       motion_lines[p][i[1]*3+3+1]=str(round(float(motion_lines[p][j[1]*3+3+1])+45,2))
+                       motion_lines[p][i[1]*3+3+2]=str(round(float(motion_lines[p][j[1]*3+3+2]),2))
+                       motion_lines[p][j[1]*3+3]=str(round(float(val_1),2))
+                       motion_lines[p][j[1]*3+3+1]=str(round(float(val_2),2))
+                       motion_lines[p][j[1]*3+3+2]=str(round(float(val_3),2))
+        elif("Left" not in i[0] and "Right" not in i[0] and "Chest" not in i[0] and "Head" not in i[0] or "Foot" in i[0] or "Ankle" in i[0] or "Knee" in i[0] or "Hip" in i[0] ):
+           #print(i)
+           motion_lines[p][i[1]*3+3]=str(0)
+           motion_lines[p][i[1]*3+3+1]=str(0)
+           motion_lines[p][i[1]*3+3+2]=str(0)
+           if("Hips" in i[0]):
+               motion_lines[p][i[1]]=str(0)
+               motion_lines[p][i[1]+1]=str(0)
+               motion_lines[p][i[1]+2]=str(0)
+            
+               
        
 for line in l:
     if("Frame Time" in line):
@@ -75,9 +89,13 @@ for line in l:
     else:
         new_bvh.write(line)
 
+index_write=0
 for k in motion_lines:
     stringa=""
     for j in k:
         stringa+=str(j)+" "
     stringa+="\n"
     new_bvh.write(stringa)
+    index_write+=1
+    #if(index_write==1000):
+    #    break
